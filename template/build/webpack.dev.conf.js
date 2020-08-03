a7dc7a4dabf8295832a0d47bcd2f8f19e478f73f
Nick Zhang <thesadboy@qq.com>
2020-08-03 10:43:33 +0800

    update

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -54,9 +54,6 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': require('../config/dev.env')
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
-    new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: 'index.html',
@@ -71,7 +68,11 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         ignore: ['.*']
       }
     ])
-  ]
+  ],
+  optimization: {
+    namedModules: true,
+    noEmitOnErrors: true
+  }
 });
 
 module.exports = new Promise((resolve, reject) => {
